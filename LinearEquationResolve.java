5207628eb75a4ced3216d28f5a9bee1f5cb9d403
GMJoker1964 <38500531+GMJoker1964@users.noreply.github.com>
2019-02-22 10:07:26 +0700

    Add files via upload

---
+++ LinearEquationResolve.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class LinearEquationResolve {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = 0', please enter constants:");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("a: ");
+        double a = scan.nextDouble();
+
+        System.out.print("b: ");
+        double b = scan.nextDouble();
+
+        System.out.print("c: ");
+        double c = scan.nextDouble();
+
+        if (a != 0) {
+            double solution = -b / a;
+            System.out.printf("The solution is: %f!", solution);
+        }
+
+        if (a != 0) {
+            double answer = (c - b) / a;
+            System.out.printf("Equation pass with x = %f!\n", answer);
+        } else {
+            if (b == 0) {
+                System.out.print("The solution is all x!");
+            } else {
+                System.out.print("No solution!");
+            }
+        }
+
+
+    }
+}
+
